8cd079dcc62cf5d2319f068b2ab8b12a0091f544
kasib <kasibzxc@gmail.com>
2023-11-01 15:35:39 -0400

    solved traversal with a list

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -14,11 +15,20 @@ class Node<T> {
 
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+
+    // way overenginered a traversal at first. There is no swapping
+    // check if node is not null and just add the value and update the node that
+    // walks from the head.
+    List<String> result = new ArrayList<>();
+    Node<String> curr = head;
+    while (curr != null) {
+      result.add(curr.val);
+      curr = curr.next;
+    }
+    return result;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -29,7 +39,7 @@ class Solution {
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    System.out.println(Solution.linkedListValues(a));
     // -> [ "a", "b", "c", "d" ]
   }
 }
